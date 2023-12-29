f67a7373564d407089db7ae7e559d8cf2e622eb9
NigelOmondi <nigelomondi62@gmail.com>
2023-12-29 15:04:37 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# StudyBuddy Application 
+The field of software engineering demands constant learning , guidance and collaboration with other experienced teams , senior developers and even peers. This is the goal of StudyBuddy, to help find study partners, friends, collaborators.
+It is a full-stack application built with HTML, CSS, JavaScript, Django framework and Django REST API.
+
+
+![login page](./images/LoginScreen.png)
+
+![home page](./images/homepage.png)
+
+![study room](./images/roomchat.png)
---
+++ base/models.py
@@ -0,0 +1,76 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Users must have an email address')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200,unique=True, null=True)
+    bio = models.TextField(null=True)
+
+    avatar = models.ImageField(null=True, default='avatar.svg')
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+# Create your models here.
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    participants = models.ManyToManyField(User, related_name='participants', blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    #To return a string representation of the class
+    def __str__(self):
+        return self.name
+    
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    
+    def __str__(self):
+        return self.body[0:50]
